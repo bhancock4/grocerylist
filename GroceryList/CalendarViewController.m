4ce540612924b3ab4a1bc0153a948cc837ef6d16
bhancock4 <bhancock4@gmail.com>
2014-11-11 19:42:43 -0600

    get calendar control centered and resized on UI.  added ckcalendarevent type to calendar testing code

--- GroceryList/CalendarViewController.m
+++ GroceryList/CalendarViewController.m
@@ -14,22 +14,26 @@
 {
     [super viewDidLoad];
     
-    // 1. Instantiate a CKCalendarView
+    //programmatially create/setup instance of the MBCalendarKit calendar/tableview object
     CKCalendarView* calendar = [[CKCalendarView alloc] initWithMode:CKCalendarViewModeMonth];
-    
-    // 2. Optionally, set up the datasource and delegates
     calendar.delegate = self;
     calendar.dataSource = self;
-    
-    // 3. Present the calendar
+    //add the calendar the the UI
     [[self view] addSubview:calendar];
+    //force centering/resizing of calendar
+    self.view.autoresizingMask = (UIViewAutoresizingFlexibleLeftMargin   |
+                                  UIViewAutoresizingFlexibleRightMargin  |
+                                  UIViewAutoresizingFlexibleTopMargin    |
+                                  UIViewAutoresizingFlexibleBottomMargin);
     
+    //keep calendar from being hidden behind navigation controller bars
     if ([self respondsToSelector:@selector(edgesForExtendedLayout)])
         self.edgesForExtendedLayout = UIRectEdgeNone;
 }
 
 - (NSArray *)calendarView:(CKCalendarView *)calendarView eventsForDate:(NSDate *)date
 {
+    //testing code...adds two bogus CKCalendarEvents on the day 5 days from now
     NSDateComponents* dateComps1 = [[NSCalendar currentCalendar] components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:date];
     
     NSDateComponents* dateComps2 = [[NSCalendar currentCalendar] components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay fromDate:[[NSDate date] dateByAddingTimeInterval:60*60*24*5]];
@@ -38,8 +42,12 @@
     NSDate* date2 = [[NSCalendar currentCalendar] dateFromComponents:dateComps2];
     
     if([date1 isEqualToDate:date2])
-        return [NSArray arrayWithObjects:@"Breakfast", @"Lunch", @"Dinner", nil];
-    
+    {
+        CKCalendarEvent* event1 = [CKCalendarEvent eventWithTitle:@"Test Event1" andDate:date andInfo:nil ];
+        CKCalendarEvent* event2 = [CKCalendarEvent eventWithTitle:@"Test Event2" andDate:date andInfo:nil ];
+        
+        return [NSArray arrayWithObjects:event1, event2, nil];
+    }
     return nil;
 }
 
